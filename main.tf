162dedb3b18f666efc0e4fa21770c3555b5ff8cf
corysell <corysell@protonmail.com>
2023-11-13 11:40:08 -0600

    specified ec2 instance resource

--- main.tf
+++ main.tf
@@ -8,11 +8,12 @@ variable "avail_zone" {}
 variable "env_prefix" {}
 variable "home_cidr" {}
 variable "any_cidr" {}
+variable "instance_type" {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
-      Name: "${var.env_prefix}-vpc"
+      Name: "${ var.env_prefix }-vpc"
     }
 }
 
@@ -21,14 +22,14 @@ resource "aws_subnet" "myapp-subnet-1" {
     cidr_block = var.subnet_cidr_block
     availability_zone = var.avail_zone
     tags = {
-      Name: "${var.env_prefix}-subnet-1"
+      Name: "${ var.env_prefix }-subnet-1"
     }
 }
 
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
   tags = {
-    Name: "${var.env_prefix}-igw"
+    Name: "${ var.env_prefix }-igw"
   }
 }
 
@@ -40,7 +41,7 @@ resource "aws_default_route_table" "main-rtb" {
     gateway_id = aws_internet_gateway.myapp-igw.id
   }
   tags = {
-    Name: "${var.env_prefix}-main-rtb"
+    Name: "${ var.env_prefix }-main-rtb"
   }
 }
 
@@ -51,25 +52,62 @@ resource "aws_default_security_group" "default-sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = [var.home_cidr]
+    cidr_blocks = [ var.home_cidr ]
   }
 
   ingress {
     from_port = 8080
     to_port = 8080
     protocol = "tcp"
-    cidr_blocks = [var.any_cidr]
+    cidr_blocks = [ var.any_cidr ]
   }
 
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = [var.any_cidr]
+    cidr_blocks = [ var.any_cidr ]
     prefix_list_ids = []
   }
 
   tags = {
     Name: "${var.env_prefix}-default-sg"
+  }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name = "name"
+    values = [ "al2023-ami-2023*-kernel-6.1-x86_64" ]
+  }
+  filter {
+    name = "virtualization-type"
+    values = [ "hvm" ]
+  }
+  filter {
+    name = "architecture"
+    values = [ "x86_64" ]
+  }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [ aws_default_security_group.default-sg.id ]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = "server-key-pair"
+
+  tags = {
+    Name = "${var.env_prefix}-server"
   }
 }
