2ff5ae299be1008349ce85e2ea8a4a1ff93dc1b7
corysell <corysell@protonmail.com>
2023-11-13 17:48:57 -0600

    created subnet module + references

--- main.tf
+++ main.tf
@@ -2,15 +2,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
-variable "home_cidr" {}
-variable "any_cidr" {}
-variable "instance_type" {}
-variable "public_key_location" {}
-
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
@@ -18,32 +9,14 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-    Name : "${var.env_prefix}-subnet-1"
-  }
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
+module "myapp-subnet" {
+  source = "./modules/subnet"
+  subnet_cidr_block = var.subnet_cidr_block
+  avail_zone = var.avail_zone
+  env_prefix = var.env_prefix
+  any_cidr = var.any_cidr
   vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name : "${var.env_prefix}-igw"
-  }
-}
-
-resource "aws_default_route_table" "main-rtb" {
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-  route {
-    cidr_block = var.any_cidr
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name : "${var.env_prefix}-main-rtb"
-  }
 }
 
 resource "aws_default_security_group" "default-sg" {
@@ -93,14 +66,6 @@ data "aws_ami" "latest-amazon-linux-image" {
   }
 }
 
-output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-linux-image.id
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
-}
-
 resource "aws_key_pair" "ssh-key" {
   key_name   = "server-key"
   public_key = file(var.public_key_location)
@@ -110,7 +75,7 @@ resource "aws_instance" "myapp-server" {
   ami           = data.aws_ami.latest-amazon-linux-image.id
   instance_type = var.instance_type
 
-  subnet_id              = aws_subnet.myapp-subnet-1.id
+  subnet_id              = module.myapp-subnet.subnet.id
   vpc_security_group_ids = [aws_default_security_group.default-sg.id]
   availability_zone      = var.avail_zone
 
