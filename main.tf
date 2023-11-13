56d68275cf17c9f4c5337b88c21fa56525eb53e5
corysell <corysell@protonmail.com>
2023-11-13 15:56:20 -0600

    created shell script for docker/nginx build

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ variable "env_prefix" {}
 variable "home_cidr" {}
 variable "any_cidr" {}
 variable "instance_type" {}
+variable "public_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -96,6 +97,15 @@ output "aws_ami_id" {
   value = data.aws_ami.latest-amazon-linux-image.id
 }
 
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name   = "server-key"
+  public_key = file(var.public_key_location)
+}
+
 resource "aws_instance" "myapp-server" {
   ami           = data.aws_ami.latest-amazon-linux-image.id
   instance_type = var.instance_type
@@ -105,7 +115,9 @@ resource "aws_instance" "myapp-server" {
   availability_zone      = var.avail_zone
 
   associate_public_ip_address = true
-  key_name                    = "server-key-pair"
+  key_name                    = aws_key_pair.ssh-key.key_name
+
+  user_data = file("entry-script.sh")
 
   tags = {
     Name = "${var.env_prefix}-server"
