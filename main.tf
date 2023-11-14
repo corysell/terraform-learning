38b1f31f791759cfe48c3e24eeb88cc95b291f4c
corysell <corysell@protonmail.com>
2023-11-13 20:37:34 -0600

    removed resources for modularization

--- main.tf
+++ main.tf
@@ -10,81 +10,24 @@ resource "aws_vpc" "myapp-vpc" {
 }
 
 module "myapp-subnet" {
-  source = "./modules/subnet"
-  subnet_cidr_block = var.subnet_cidr_block
-  avail_zone = var.avail_zone
-  env_prefix = var.env_prefix
-  any_cidr = var.any_cidr
-  vpc_id = aws_vpc.myapp-vpc.id
+  source                 = "./modules/subnet"
+  subnet_cidr_block      = var.subnet_cidr_block
+  avail_zone             = var.avail_zone
+  env_prefix             = var.env_prefix
+  any_cidr               = var.any_cidr
+  vpc_id                 = aws_vpc.myapp-vpc.id
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
-resource "aws_default_security_group" "default-sg" {
-  vpc_id = aws_vpc.myapp-vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.home_cidr]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = [var.any_cidr]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = [var.any_cidr]
-    prefix_list_ids = []
-  }
-
-  tags = {
-    Name : "${var.env_prefix}-default-sg"
-  }
-}
-
-data "aws_ami" "latest-amazon-linux-image" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023*-kernel-6.1-x86_64"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-}
-
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "server-key"
-  public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "myapp-server" {
-  ami           = data.aws_ami.latest-amazon-linux-image.id
-  instance_type = var.instance_type
-
-  subnet_id              = module.myapp-subnet.subnet.id
-  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-  availability_zone      = var.avail_zone
-
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.ssh-key.key_name
-
-  user_data = file("entry-script.sh")
-
-  tags = {
-    Name = "${var.env_prefix}-server"
-  }
+module "myapp-webserver" {
+  source              = "./modules/webserver"
+  instance_type       = var.instance_type
+  home_cidr           = var.home_cidr
+  any_cidr            = var.any_cidr
+  env_prefix          = var.env_prefix
+  avail_zone          = var.avail_zone
+  public_key_location = var.public_key_location
+  vpc_id              = aws_vpc.myapp-vpc.id
+  subnet_id           = module.myapp-subnet.subnet.id
+  image_name          = var.image_name
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,36 @@
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
-variable "home_cidr" {}
-variable "any_cidr" {}
-variable "instance_type" {}
-variable "public_key_location" {}
+variable "vpc_cidr_block" {
+  description = "VPC supernet cidr block"
+  type        = string
+}
+variable "subnet_cidr_block" {
+  description = "subnet cidr block for myapp"
+  type        = string
+}
+variable "avail_zone" {
+  description = "availability zone for myapp deployment"
+  type        = string
+}
+variable "env_prefix" {
+  description = "environment prefix for myapp, eg. dev, prod, etc."
+  type        = string
+}
+variable "home_cidr" {
+  description = "home cidr block for security group rules"
+  type        = string
+}
+variable "any_cidr" {
+  description = "any cidr block, 0.0.0/0"
+  type        = string
+}
+variable "instance_type" {
+  description = "EC2 instance type being specified for creation"
+  type        = string
+}
+variable "public_key_location" {
+  description = "public rsa_id key location for SSH connectivity"
+  type        = string
+}
+variable "image_name" {
+  description = "AMI image file for EC2 instance creation"
+  type        = string
+}
